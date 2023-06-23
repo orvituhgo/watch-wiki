7490f144f03396463f15bf8eec74b116a9b688dd
orvituhgo <orvituhgo@gmail.com>
2023-06-23 01:54:59 -0300

    alter title logo

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -8,7 +8,7 @@ export default function ToolbarMenu() {
             <section className={style.section}>
 
                 <div className={style.horizontalLine}></div>
-                <h1 className={style.title}> WATCH HUB</h1>
+                <h1 className={style.title}> watch HUB</h1>
                 <div className={style.horizontalLine}></div>
             </section>
         </>
